13ab50d3e1d30301aef081b6f9679df097ce6d73
Michael Friedman <contact@michaeldavidfriedman.com>
2016-06-20 23:39:48 -0700

    Wrote methods for coffee projections.

--- app.js
+++ app.js
@@ -26,3 +26,35 @@ function CoffeeShop(name, minCustHr, maxCustHr, cupsCust, poundsCust) {
   allItems.push(this);
   stores.push(this.name);
 }
+//functions and methods
+function getRandomInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//methods
+CoffeeShop.prototype.getRandomCustomer = function() {
+  for (var j = 0; j < businessHours.length; j++) {
+    this.hourlyCustomers.push(getRandomInclusive(this.minCustHr, this.maxCustHr));
+  }
+};
+CoffeeShop.prototype.getCupsSold = function () {
+  for (var k = 0; k < this.hourlyCustomers.length; k++) {
+    this.hourlycutomers = this.hourlyCustomers[k];
+    this.cupsSold.push((this.hourlyCustomers[k] * this.cupsCust));
+  }
+};
+CoffeeShop.prototype.getToGoBeansSold = function() {
+  for (var l = 0; l < this.hourlyCustomers.length; l++) {
+    this.hourlycutomers = this.hourlyCustomers[l];
+    this.toGoBeansSold.push((this.poundsCust * this.hourlyCustomers[l]));
+  }
+};
+CoffeeShop.prototype.getBeansForCups = function() {
+  for (var m = 0; m < this.cupsSold.length; m++) {
+    this.beansForCups.push((this.cupsSold[m] / 16));
+  }
+};
+CoffeeShop.prototype.getEmployeesPerHour = function() {
+  for (var o = 0; o < this.hourlyCustomers.length; o++) {
+    this.employeesPerHour.push(Math.ceil(this.cupsSold[o] / 30));
+  }
+};
